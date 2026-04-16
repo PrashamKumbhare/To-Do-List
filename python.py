94dd944dff03799ee0ec0f817470d88e55f1e17c
Prasham Kumbhare <prashamkumbhare2@gmail.com>
2026-04-16 13:14:48 +0530

    Add files via upload

---
+++ python.py
@@ -0,0 +1,9 @@
+print("Enter Today's Three Tasks")
+task1 = input("Enter 1st Task:")
+task2 = input("Enter 2nd Task:")
+task3 = input("Enter 3rd Task:")
+todo = [task1,task2,task3]
+print("Today's Tasks:")
+print(todo[0])
+print(todo[1])
+print(todo[2])
